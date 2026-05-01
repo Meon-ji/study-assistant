64922ee6de04d709617502e1c07eff7422fbb3fb
Parkyunseo (PC yunseo-40) <pys33129747@ajou.ac.kr>
2026-05-02 04:17:32 +0900

    refactor: 입력값 검증 수정 및 에러 status 수정

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -9,18 +9,26 @@ export async function registerService ({ email, name, password }) {
         throw error;
     }
 
+    //패스워드 길이 검사
+    if(password.length < 8){
+        const error = new Error("패스워드의 길이는 8자 이상이어야 합니다.");
+        error.status = 400;
+        throw error;
+    }
+
     //이메일 중복 검사
     const existingUser = await prisma.user.findUnique({
         where: {email},
     });
     if(existingUser){
         const error = new Error("이미 존재하는 이메일입니다.");
-        error.status = 400;
+        error.status = 409;
         throw error;
     }
 
     //비밀번호 해싱
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const SALT_ROUNDS = 10;
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
     //user 생성
     const user = await prisma.user.create({
